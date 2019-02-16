a4fbef0faeb5c0ff592a30bd2adf813f777de507
Administrator <Administrator@192.168.8.103>
2019-02-16 22:15:25 +0800

    cloudfront update

--- src/main/java/com/xxss/config/CloudFrontConfig.java
+++ src/main/java/com/xxss/config/CloudFrontConfig.java
@@ -1,6 +1,6 @@
 package com.xxss.config;
 
 public class CloudFrontConfig {
-	public static final String distributionDomain = "dkxh721496qhs.cloudfront.net";
+	public static final String distributionDomain = "91xxss.info";
 	public static final String privateKeyFilePath = "C:\\Users\\Administrator\\Desktop\\pk-APKAISHG6BZ5RZ6W43UA.pem";
 }
