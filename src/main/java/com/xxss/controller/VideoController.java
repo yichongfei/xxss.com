5b8dc0c1d3685a27ad86f4399e508aa564c8641c
Administrator <Administrator@192.168.8.105>
2019-04-03 14:54:11 +0800

    123

--- src/main/java/com/xxss/controller/VideoController.java
+++ src/main/java/com/xxss/controller/VideoController.java
@@ -1,5 +1,7 @@
 package com.xxss.controller;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Calendar;
 import java.util.List;
 import java.util.Random;
@@ -152,24 +154,42 @@ public class VideoController {
 	
 	public String getIpAddr(HttpServletRequest request) {
 		String ip = request.getHeader("x-forwarded-for");
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("http_client_ip");
-		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getRemoteAddr();
-		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+		if (ip == null || ip.length() == 0
+				|| "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("Proxy-Client-IP");
 		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+		if (ip == null || ip.length() == 0
+				|| "unknown".equalsIgnoreCase(ip)) {
 			ip = request.getHeader("WL-Proxy-Client-IP");
 		}
-		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
-			ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+		if (ip == null || ip.length() == 0
+				|| "unknown".equalsIgnoreCase(ip)) {
+			ip = request.getRemoteAddr();
+			if (ip.equals("127.0.0.1")||ip.equals("0:0:0:0:0:0:0:1")) {
+				// 根据网卡取本机配置的IP
+				InetAddress inet = null;
+				try {
+					inet = InetAddress.getLocalHost();
+				} catch (UnknownHostException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				ip = inet.getHostAddress();
+
+				// 对于通过多个代理的情况，第一个IP为客户端真实IP,多个IP按照','分割
+				if (ip != null && ip.length() > 15) { // "***.***.***.***".length()
+														// = 15
+					if (ip.indexOf(",") > 0) {
+						ip = ip.substring(0, ip.indexOf(","));
+					}
+				}
+			}
 		}
-		// 如果是多级代理，那么取第一个ip为客户ip
-		if (ip != null && ip.indexOf(",") != -1) {
-			ip = ip.substring(ip.lastIndexOf(",") + 1, ip.length()).trim();
+		if (ip != null && ip.length() > 15) { // "***.***.***.***".length()
+												// = 15
+			if (ip.indexOf(",") > 0) {
+				ip = ip.substring(0, ip.indexOf(","));
+			}
 		}
 		return ip;
 	}
