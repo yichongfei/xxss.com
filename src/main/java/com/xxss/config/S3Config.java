c43bd97fdc630b8e37a6f2ef8b6ae25826763bba
Administrator <Administrator@192.168.8.105>
2019-04-03 14:46:07 +0800

    123

--- src/main/java/com/xxss/config/S3Config.java
+++ src/main/java/com/xxss/config/S3Config.java
@@ -11,7 +11,7 @@ public class S3Config {
 	
 	public static final String OWNER = "这里好玩不";
 	
-	public static final int playTimesWithOutAccount =3;
+	public static final int playTimesWithOutAccount =1;
 	
 	public static final int playTimesForFree = 3;
 	
